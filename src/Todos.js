ede3d56fd241211772d089b87a6e292ffe675d9a
mc-santiago <mari.c.santiago@europe.com>
2019-10-29 17:07:55 +0100

    todo list displayed and delete function working

---
+++ src/Todos.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Todos = ({todos, deleteTodo}) => {
+
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return(
+                <div className="collection-item" key={todo.id}>
+                    <p onClick={() => {deleteTodo(todo.id)}}>{todo.content}</p>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center"> 
+            It looks like you are done for the week. Now get out of here and go have a caña or something!
+        </p>
+    )
+    
+    return (
+        <div className="todos collection">
+            {todoList}
+        </div>
+    )
+}
+
+export default Todos;
